01a3cbe603aee653327bf8983bb8e26642df77c6
hallroger <hallroger@users.noreply.github.com>
2022-06-14 23:53:11 +0700

    Second

--- heroku/code.py
+++ heroku/code.py
@@ -3,87 +3,63 @@ import pandas as pd
 from bokeh.io import output_file, show, output_notebook, save
 from bokeh.models import ColumnDataSource, Select, DateRangeSlider
 from bokeh.plotting import figure, show
-from bokeh.models import CustomJS, HoverTool
+from bokeh.models import CustomJS, HoverTool, ColumnDataSource
 from bokeh.layouts import column
+from bokeh.palettes import Spectral6
 
-import flask
-import os
-from flask import send_from_directory
+#Nampilin File Output
+output_file('Tubes_Visdat.html')
 
-app = flask.Flask(__name__)
+#Read CSV
+df = pd.read_csv("datasettubes.csv")
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
+Negara_list = list(df['Negara'].unique())
 
-@app.route('/')
-@app.route('/home')
-def home():
-	df = pd.read_csv("datasetfix.csv")
-	Location_list = list(df['Location'].unique())
+df['Tahun'] = pd.to_datetime(df['Tahun'])
 
-	df['Date'] = pd.to_datetime(df['Date'])
+cols1=df.loc[:, ['Negara','Tahun', 'Export']]
+cols2 = cols1[cols1['Negara'] == 'Malaysia' ]
 
-	cols1=df.loc[:, ['Location','Date', 'Total Active Cases','Total Deaths', 'Total Recovered', 'Total Cases']]
-	cols2 = cols1[cols1['Location'] == 'DKI Jakarta' ]
+Overall = ColumnDataSource(data=cols1)
+Curr=ColumnDataSource(data=cols2)
 
-	Overall = ColumnDataSource(data=cols1)
-	Curr=ColumnDataSource(data=cols2)
+callback = CustomJS(
+	args=dict(source=Overall, sc=Curr), 
+	code="""
+		var f = cb_obj.value
+		sc.data['Tahun']=[]
+		sc.data['Export']=[]
+		for(var i = 0; i <= source.get_length(); i++){
+			if (source.data['Negara'][i] == f){
+				sc.data['Tahun'].push(source.data['Date'][i])
+                sc.data['Export'].push(source.data['Export'][i])
+               
+			 }
+		}   
+		   
+		sc.change.emit();
+		"""
+)
 
-	callback = CustomJS(
-		args=dict(source=Overall, sc=Curr), 
-		code="""
-			var f = cb_obj.value
-			sc.data['Date']=[]
-			sc.data['Total Cases']=[]
-			sc.data['Total Deaths']=[]
-			sc.data['Total Recovered']=[]
-			sc.data['Total Active Cases']=[]
-			for(var i = 0; i <= source.get_length(); i++){
-				if (source.data['Location'][i] == f){
-					sc.data['Date'].push(source.data['Date'][i])
-					sc.data['Total Cases'].push(source.data['Total Recovered'][i])
-					sc.data['Total Deaths'].push(source.data['Total Recovered'][i])
-					sc.data['Total Recovered'].push(source.data['Total Recovered'][i])
-					sc.data['Total Active Cases'].push(source.data['Total Active Cases'][i])
-				}
-			}   
-			
-			sc.change.emit();
-			"""
-	)
+menu = Select(options=Negara_list,value='Malaysia', title = 'Negara')  # drop down menu
+bokeh_p=figure(x_axis_label ='Tahun', y_axis_label = 'Total Export (Ton)', y_axis_type="linear",x_axis_type="datetime") #creating figure object 
+bokeh_p.line(x='Tahun', y='Export', color='Red', legend_label="Total Export (Ton)",source=Curr)
+bokeh_p.legend.location = "top_right"
 
-	menu = Select(options=Location_list,value='DKI Jakarta', title = 'Location')  # drop down menu
-	bokeh_p=figure(x_axis_label ='Date', y_axis_label = 'Total Active Cases', y_axis_type="linear",x_axis_type="datetime") #creating figure object 
-	bokeh_p.line(x='Date', y='Total Cases', color='green', legend_label="Total Kasus",source=Curr)
-	bokeh_p.line(x='Date', y='Total Deaths', color='black', legend_label="Total Kematian",source=Curr) 
-	bokeh_p.line(x='Date', y='Total Recovered', color='blue', legend_label="Total Sembuh",source=Curr) 
-	bokeh_p.line(x='Date', y='Total Active Cases', color='red',legend_label="Total Kasus Aktif", source=Curr)
-	bokeh_p.legend.location = "top_right"
+bokeh_p.add_tools(HoverTool(
+    tooltips=[
+        ('Export (Ton)', '@{Export}'),
+    ],
 
-	bokeh_p.add_tools(HoverTool(
-		tooltips=[
-			('Total Kasus', '@{Total Cases}'),
-			('Total Kematian', '@{Total Deaths}'),
-			('Total Sembuh', '@{Total Recovered}'),
-			('Total Kasus Aktif', '@{Total Active Cases}'),
-		],
+    mode='mouse'
+))
 
-		mode='mouse'
-	))
+menu.js_on_change('value', callback) 
 
-	menu.js_on_change('value', callback) 
+date_range_slider = DateRangeSlider(value=(min(df['Tahun']), max(df['Tahun'])),start=min(df['Tahun']), end=max(df['Tahun']))
 
-	date_range_slider = DateRangeSlider(value=(min(df['Date']), max(df['Date'])),start=min(df['Date']), end=max(df['Date']))
+date_range_slider.js_link("value", bokeh_p.x_range, "start", attr_selector=0)
+date_range_slider.js_link("value", bokeh_p.x_range, "end", attr_selector=1)
+layout = column(menu, date_range_slider, bokeh_p)
 
-	date_range_slider.js_link("value", bokeh_p.x_range, "start", attr_selector=0)
-	date_range_slider.js_link("value", bokeh_p.x_range, "end", attr_selector=1)
-	layout = column(menu, date_range_slider, bokeh_p)
-
-	show(layout)
-
-if __name__ == "__main__":
-    app.secret_key = 'ItIsASecret'
-    app.debug = True
-    app.run()
+show(layout)
